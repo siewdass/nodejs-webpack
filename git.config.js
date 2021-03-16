84889c30df9c196d1a8c6a3efaef1760742bb889
siewdass <siewdass@gmail.com>
2021-03-15 20:56:54 -0400

    test

--- git.config.js
+++ git.config.js
@@ -1,24 +1,37 @@
 const config = require( './package.json' )
 const exec = require( 'child_process' ).exec
-const readline = require('readline').createInterface({
+const readline = require( 'readline' )
+
+const rl = readline.createInterface( {
   input: process.stdin,
   output: process.stdout
-})
-
-var cmd = null
+} )
+var cmd = ''
 
 version = config[ 'version' ]
 
 if ( process.argv.includes( 'pull' ) ) {
 	cmd = 'git pull'
 } else if ( process.argv.includes( 'push' ) ) {
-	cmd = 'git add . && git commit -m "-" && git push'
+	rl.question( '', ( commit ) => {
+		cmd = 'git add . && git commit -m "'+ commit +'" && git push '
+		console.log( '\033[1A' + cmd )
+
+		exec( cmd, ( error, stdOut, stdErr ) => {
+			if ( error == null ) {
+				//console.log( stdOut.slice( 0, -1 ) )
+				console.log( '\033[1A' + cmd )
+			}
+		} )
+		rl.close( )
+	} )
 } else {
 	process.exit( )
 }
 
+/*process.exit( )
 exec( cmd, ( error, stdOut, stdErr ) => {
 	if ( error == null ) {
 		console.log( stdOut.slice( 0, -1 ) )
 	}
-} )
+} )*/
