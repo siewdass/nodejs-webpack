504e8bc60d252f555802c3b802db0a635567ce08
siewdass <siewdass@gmail.com>
2021-03-15 20:19:08 -0400

    -

--- repo.js
+++ repo.js
@@ -1,11 +1,16 @@
-var exec = require( 'child_process' ).exec
+const config = require( './package.json' )
+const exec = require( 'child_process' ).exec
 
 var cmd = null
+
+version = config[ 'version' ]
 
 if ( process.argv.includes( 'pull' ) ) {
 	cmd = 'git pull'
 } else if ( process.argv.includes( 'push' ) ) {
 	cmd = 'git add . && git commit -m "-" && git push'
+} else {
+	process.exit( )
 }
 
 exec( cmd, function ( error, stdOut, stdErr ) {
