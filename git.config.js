8ebec3ad7874807d78b1d454063476ea3a102470
siewdass <siewdass@gmail.com>
2021-03-15 20:58:21 -0400

    jaja

--- git.config.js
+++ git.config.js
@@ -11,12 +11,14 @@ var cmd = ''
 version = config[ 'version' ]
 
 if ( process.argv.includes( 'pull' ) ) {
-	cmd = 'git pull'
+	exec( 'git pull', ( error, stdOut, stdErr ) => {
+		if ( error == null ) {
+			console.log( '\033[1A' + stdOut.slice( 0, -1 ) )
+		}
+	} )
 } else if ( process.argv.includes( 'push' ) ) {
 	rl.question( '', ( commit ) => {
 		cmd = 'git add . && git commit -m "'+ commit +'" && git push '
-		//console.log( '\033[1A' + cmd )
-
 		exec( cmd, ( error, stdOut, stdErr ) => {
 			if ( error == null ) {
 				console.log( '\033[1A' + stdOut.slice( 0, -1 ) )
@@ -27,10 +29,3 @@ if ( process.argv.includes( 'pull' ) ) {
 } else {
 	process.exit( )
 }
-
-/*process.exit( )
-exec( cmd, ( error, stdOut, stdErr ) => {
-	if ( error == null ) {
-		console.log( stdOut.slice( 0, -1 ) )
-	}
-} )*/
