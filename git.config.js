6efc0e75cb3b1f2f76fd51745d95f537e2528f5e
siewdass <siewdass@gmail.com>
2021-03-16 07:29:27 -0400

    don't know

--- git.config.js
+++ git.config.js
@@ -1,20 +1,23 @@
 const config = require( './package.json' )
-const proc = require( 'child_process' )
+const child_process = require( 'child_process' )
 const readline = require( 'readline' )
 
-const exec = proc.exec 
-const input = readline.createInterface( { input: process.stdin, output: process.stdout } )
+const exec = child_process.exec 
+const input = readline.createInterface( { 
+	input: process.stdin, output: process.stdout
+} )
 
 version = config[ 'version' ]
 
 if ( process.argv.includes( 'pull' ) ) {
 	exec( 'git pull', ( error, stdOut, stdErr ) => {
 		if ( error == null ) {
-			console.log( '\033[1A' + stdOut.slice( 0, -1 ) )
+			console.log( stdOut.slice( 0, -1 ) )
+			process.exit( )
 		}
 	} )
 } else if ( process.argv.includes( 'push' ) ) {
-	input.question( '', ( commit ) => {
+	input.question( 'Commit: ', ( commit ) => {
 		var cmd = 'git add . && git commit -m "'+ commit +'" && git push '
 		exec( cmd, ( error, stdOut, stdErr ) => {
 			if ( error == null ) {
